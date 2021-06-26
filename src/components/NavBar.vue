adb34f57a18c90b476e7317c5dfaaec883fa8141
JulianWan1 <needmail.julian@gmail.com>
2021-06-26 17:46:46 +0800

    Add Navbar to the main page & modified page design

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="nav-bar-container">
+    <b-navbar>
+      <template #start>
+        <b-navbar-item>
+          JULIAN WAN
+        </b-navbar-item>
+      </template>
+      <template #end>
+        <b-navbar-item>
+          ABOUT
+        </b-navbar-item>
+        <b-navbar-item>
+          PROJECTS
+        </b-navbar-item>
+        <b-navbar-item>
+          CONTACT
+        </b-navbar-item>    
+      </template>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class NavBar extends Vue{
+}
+</script>
+
+<style scoped>
+.nav-bar-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 30px;
+  padding-left: 60px;
+  padding-right: 60px;
+  padding-top: 20px;
+}
+
+</style>
