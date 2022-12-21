edc075c5fb073d1fe8cd20610ac8ff471f348815
Julian Wan <julianwan@Julians-MBP.realtek>
2022-12-21 23:33:31 +0800

    recreate dist and set contact icon opacity to decimal

--- src/models/contact.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Contacts {
-	image: string,
-	linkExists: boolean,
-	link?: string | undefined,
-	description?: string,
-}
--- src/models/experience.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Technologies {
-	name:string,
-	image:string,
-}
